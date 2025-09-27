224f0f5364d3f09d160034e5f135be74396c938b
IagoMoreira26 <iagomoreira2353@gmail.com>
2025-09-26 22:43:58 -0300

    feat: Project Created

---
+++ src/test/java/com/WebServiceProject/Web/Service/Project/WebServiceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WebServiceProject.Web.Service.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
