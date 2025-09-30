794d1bf5873f8272421465181a7f3ede8bc25939
IagoMoreira26 <iagomoreira2353@gmail.com>
2025-09-30 11:04:48 -0300

    feat: Enum OrderStatus

--- src/main/java/com/WebServiceProject/Web/Service/Project/config/TestConfig.java
+++ src/main/java/com/WebServiceProject/Web/Service/Project/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.WebServiceProject.Web.Service.Project.entities.Order;
 import com.WebServiceProject.Web.Service.Project.entities.User;
+import com.WebServiceProject.Web.Service.Project.entities.enums.OrderStatus;
 import com.WebServiceProject.Web.Service.Project.repositories.OrderRepository;
 import com.WebServiceProject.Web.Service.Project.repositories.UserRepository;
 
@@ -29,8 +30,8 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 
-		Order o1 = new Order(null, Instant.parse("2025-09-29T20:16:00Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2025-09-29T20:16:00Z"), u2);
+		Order o1 = new Order(null, Instant.parse("2025-09-29T20:16:00Z"), OrderStatus.PAID, u1);
+		Order o2 = new Order(null, Instant.parse("2025-09-29T20:16:00Z"), OrderStatus.WAITING_PAYMENT, u2);
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2));
