9ed449bc0ce5cc8e02bf59164caacca3ba449738
IagoMoreira26 <iagomoreira2353@gmail.com>
2025-10-05 19:10:50 -0300

    feat: Implemented the OrderItem entity and its association between Product and Order

---
+++ src/main/java/com/WebServiceProject/Web/Service/Project/repositories/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.WebServiceProject.Web.Service.Project.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.WebServiceProject.Web.Service.Project.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+	
+}
